6491cd75c3a80b098606d9a36cfe843a99ea6645
K7IT2024 <dhanyavani1991@gmail.com>
2026-08-29 16:44:35 +0530

    Added render file for frontend

---
+++ web/firebase-config.js
@@ -0,0 +1,12 @@
+// Replace the values below with your Firebase Web config (safe to be public)
+// Example usage: window.__FIREBASE_WEB_CONFIG = { apiKey: '...', authDomain: '...', projectId: '...', storageBucket: '...', messagingSenderId: '...', appId: '...', measurementId: '...' };
+
+window.__FIREBASE_WEB_CONFIG = {
+  "apiKey": "REPLACE_WITH_YOUR_API_KEY",
+  "authDomain": "REPLACE_WITH_YOUR_PROJECT.firebaseapp.com",
+  "projectId": "REPLACE_WITH_YOUR_PROJECT",
+  "storageBucket": "REPLACE_WITH_YOUR_PROJECT.appspot.com",
+  "messagingSenderId": "REPLACE_WITH_YOUR_SENDER_ID",
+  "appId": "REPLACE_WITH_YOUR_APP_ID",
+  "measurementId": "REPLACE_WITH_YOUR_MEASUREMENT_ID"
+};
